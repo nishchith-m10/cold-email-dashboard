1982baf13ebeb9e4e879d60a7a29adf4b183bd37
nishchith-m10 <228442520+nishchith-m10@users.noreply.github.com>
2026-02-27 02:43:58 -0800

    feat: [SBX-005] - Install @xyflow/react + Scaffold Flow Directory

---
+++ components/sandbox/flow/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Sandbox Flow Components
+ *
+ * Barrel export for the flow-based workflow visualization engine.
+ *
+ * @module components/sandbox/flow
+ */
+
+// Components will be exported here as they are created in subsequent stories.
+// SBX-007: Custom node components
+// SBX-008: WorkflowCanvas
+// SBX-009: WorkflowSelector
+// SBX-010: NodeDetailDrawer
+// SBX-011: NodeEditForm
+// SBX-018: ExecutionTimeline
+// SBX-019: ExecutionHistory
+// SBX-022: TestRunModal
+
+export {};
